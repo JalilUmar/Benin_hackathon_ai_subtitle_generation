59469f200525a43968a61634cf46c96996bc6d0c
JalilUmar <jalil786.umer@gmail.com>
2024-05-15 16:12:28 +0500

    added tagline to logo

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -5,7 +5,10 @@ export default function Header() {
   return (
     <div className="w-full fixed top-0 bg-white text-black flex justify-center items-center ">
       <Image src={"/logo.png"} alt="logo" width={100} height={100} />
-      <h1 className="text-4xl font-bold font-sans">CulturalChords</h1>
+      <span>
+        <h1 className="text-4xl font-bold font-sans">CulturalChords</h1>
+        <p className="gray">Music without bounderies</p>
+      </span>
     </div>
   );
 }
